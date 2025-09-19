652c9505d1b39fde742d9991a218906eda2f5f48
Tanya Shah <staav410@gmail.com>
2025-09-19 02:35:24 +0000

    09/16/2025 finished roomba code

--- robot/Roomba.java
+++ robot/Roomba.java
@@ -7,12 +7,14 @@ public class Roomba implements Directions {
     // Main method to make this self-contained
     public static void main(String[] args) {
         // LEAVE THIS ALONE!!!!!!
-        String worldName = "robot/basicRoom.wld";
-        //String worldName = "robot/testWorld1.wld";
+        //String worldName = "robot/basicRoom.wld";
+        //String worldName = "robot/testWorld1.wld";;
+        String worldName = "robot/finalTestWorld.wld";
 
         Roomba cleaner = new Roomba();
-        int totalBeepers = cleaner.cleanRoom(worldName, 7, 6);
+        //int totalBeepers = cleaner.cleanRoom(worldName, 7, 6);
         //int totalBeepers = cleaner.cleanRoom(worldName, 25, 11);
+        int totalBeepers = cleaner.cleanRoom(worldName, 26, 101);
         System.out.println("Roomba cleaned up a total of " + totalBeepers + " beepers.");
 
     }
@@ -30,7 +32,7 @@ public class Roomba implements Directions {
 
         World.readWorld(worldName);
         World.setVisible(true);
-        World.setDelay(0);
+        World.setDelay(100);
         roomba = new Robot(startX, startY, East, 0);
 
         /**
@@ -41,6 +43,8 @@ public class Roomba implements Directions {
 
         // the line below causes a null pointer exception
         // what is that and why are we getting it?
+
+        //initialization of all my variables
         int totalBeepers = 0;
         int i = 0;
         int max = 0;
@@ -51,12 +55,15 @@ public class Roomba implements Directions {
         int avenue = 0;
         double averagePileSize = 0;
         double percentDirty = 0;
+        //start of while loop where it loops over to cover the whole space
         while (true) {
             numOfBeepers = 0;
+            //roomba movement if statement
             if (!roomba.nextToABeeper()) {
                 roomba.move();
                 numOfSpaces++;
             }
+            //roomba picking up beepers while loop
             while (roomba.nextToABeeper()) {
                 roomba.pickBeeper();
                 numOfBeepers++;
@@ -67,9 +74,11 @@ public class Roomba implements Directions {
                 }
                 totalBeepers++;
             }
+            //numOfPiles being calculated if statement
             if (numOfBeepers > 0) {
                 numOfPiles++;
             }
+            //roomba turning left and going up in a zig zag way
             if (!roomba.frontIsClear()) {
                 i++;
                 if (i % 2 == 1) {
@@ -80,6 +89,7 @@ public class Roomba implements Directions {
                     roomba.move();
                     numOfSpaces++;
                     roomba.turnLeft();
+                //roomba turning right and going up in a zig zag way
                 } else {
                     roomba.turnLeft();
                     roomba.turnLeft();
@@ -96,33 +106,18 @@ public class Roomba implements Directions {
                 }
             }                
         }
-        /*
-         * roomba.move();
-         * roomba.move();
-         * while (roomba.nextToABeeper()){
-         * 
-         * roomba.pickBeeper();
-         * totalBeepers++;
-         * }
-         * 
-         * roomba.move();
-         * 
-         * while (roomba.nextToABeeper()){
-         * 
-         * roomba.pickBeeper();
-         * totalBeepers++;
-         * }
-         */
 
-        // This method should return the total number of beepers cleaned up.
-        averagePileSize = totalBeepers / numOfPiles;
-        percentDirty = numOfPiles / numOfSpaces;
+        averagePileSize = (double)totalBeepers / numOfPiles;
+        percentDirty = (double)numOfPiles / numOfSpaces;
+        //Prints out area, number of Piles, largest pile, location of the largest pile, average pile size, and percent dirty.
         System.out.println("The total number of spaces is " + numOfSpaces);
         System.out.println("The number of piles is " + numOfPiles);
-        System.out.println("The max beepers is " + max);
+        System.out.println("The largest pile is " + max);
         System.out.println("The location is (" + street + "," + avenue + ")");
         System.out.println("The average pile size is "  + averagePileSize); //6.555..
         System.out.println("The percent dirty " + percentDirty); //0.225
+
+        // This method should return the total number of beepers cleaned up
         return totalBeepers;
     }
 }
