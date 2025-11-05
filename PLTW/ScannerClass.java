ab01b4a123a0d9eb5409d270bc2f9e55af258ca9
Tanya Shah <staav410@gmail.com>
2025-11-05 22:36:06 +0000

    11/05 1.2.9 PLTW Scanner

--- PLTW/ScannerClass.java
+++ PLTW/ScannerClass.java
@@ -1,14 +1,9 @@
 package PLTW;
 
-import java.io.InputStream;
 import java.util.*;
-public class Scanner {
-    public Scanner(InputStream in) {
-        //TODO Auto-generated constructor stub
-    }
-
+public class ScannerClass {
     public static void main (String [] args){
-        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Enter player 1's name here: ");
         String name1 =  scanner.nextLine();
         System.out.println("Enter player 2's name here: ");
@@ -17,14 +12,8 @@ public class Scanner {
         int age1 = scanner.nextInt();
         System.out.println("Enter player 2's age here: ");
         int age2 = scanner.nextInt();
-        scanner.nextLine(); // Consume the newline character
         
         System.out.println("Welcome " + name1 + " to the game! " + "We are delighted to have someone who is " + age1 + " years old.");
         System.out.println("Welcome " + name2 + " to the game! " + "We are delighted to have someone who is " + age2 + " years old.");
-    }
-
-    public String nextLine() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'nextLine'");
     }
 }
