0d814aa886f7e6bbf39e1417adc7e23c9aeb63b0
Tanya Shah <staav410@gmail.com>
2025-11-18 05:47:39 +0000

    11/17 First day of Card project. Finished making the Card class, editted the App class to debug and figure out what was wrong with my Deck constructor, and finished making the Deck constructor.

--- cards/Card.java
+++ cards/Card.java
@@ -2,5 +2,33 @@ package cards;
 
 public class Card {
 
+    private int suit;
+    private int value;
+
+    public Card(int suit, int value) {
+        if(suit < 0){
+            suit = 0;
+        } else if(suit > 3){
+            suit = 3;
+        }
+        if(value < 0){
+            value = 0;
+        } else if(value > 12){
+            value = 12;
+        }
+        this.suit = suit;
+        this.value = value;
+    }
+
+    public int getValue(){
+        return value;
+    }
+
+    public String toString(){
+        String [] suits = {"♦", "♣", "♥", "♠"};
+        String [] values = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+        String result = suits[suit] + values[value];
+        return result;
+    }
 
 }
--- cards/Deck.java
+++ cards/Deck.java
@@ -2,5 +2,16 @@ package cards;
 
 public class Deck {
 
+    public Deck(){
+        Card [] cardAry = new Card[52];
+        int x = 0;
+        for(int i = 0; i < 4; i++){
+            for(int j = 0; j < 13; j++){
+                cardAry[x] = new Card(i, j);
+                //System.out.println(cardAry[x]);
+                x++;
+            }
+        }
+    }
 
 }
