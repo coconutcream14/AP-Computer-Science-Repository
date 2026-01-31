8782f91a771cbbe8cb3879097a90c4c71c30a39d
Tanya Shah <staav410@gmail.com>
2026-01-31 21:44:54 +0000

    01/31 Finished code for Selection sort

--- sorting/SelectionSort.java
+++ sorting/SelectionSort.java
@@ -2,7 +2,27 @@ package sorting;
 
 public class SelectionSort implements Sorter {
 
-    public void sort(int[] input) {
-        System.out.println("Selection Sort!!!");
+    public void sort(int[] ary) {
+        int [] unsortedAry = new int[ary.length];
+        int min = ary[0];
+        for(int j = 0; j < ary.length; j++){
+            for(int i = 0; i < ary.length; i++){
+                if(min > ary[i] && ary[i] != 0){
+                    min = ary[i];
+                }
+                //System.out.println(min);
+            }
+            unsortedAry[j] = min;
+            for(int b = 0; b < ary.length; b++){
+                if(ary[b] == min){
+                    ary[b] = 0;
+                }
+            }
+            min = ary[0];
+        }
+        for(int w = 0; w < ary.length; w++){
+            ary[w] = unsortedAry[w];
+            System.out.print(ary[w] + ", ");
+        }
     }
 }
