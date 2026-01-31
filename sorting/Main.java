4bdf171f73cfd18495d5a8801df0c296acb126be
Tanya Shah <staav410@gmail.com>
2026-01-31 22:32:08 +0000

    01/31 Edited a small mistake in Selection Sort and wrote the randomArray Code for the main method

--- sorting/Main.java
+++ sorting/Main.java
@@ -1,5 +1,7 @@
 package sorting;
 
+import java.math.*;
+
 public class Main {
     public static void main(String[] args)
     {
@@ -13,18 +15,26 @@ public class Main {
         //     of InsertionSort and Selection sort continue to work.
         //  4. Increase the loop count for TestSuite to get better performance results.
 
-        int[] testInput = randomArray(5);
+        int length = 0;
+        int[] testInput;
+        for(int i = 1; i < 9; i*=2){
+            length = i * 100;
+            testInput = randomArray(length);
+            TestSuite.run(testInput, 1000);
+        }
 
         // Increase the loop count to get better results once it works.
         // TestSuite.run(testInput, 10000);
-        TestSuite.run(testInput, 1);
     }
 
     public static int[] randomArray(int length)
     {
         // TODO: make this return an actual random array of the provided length.
-        int[] a = {10, 5, 3, 2};
-        return a;
+        int[] ary = new int[length];
+        for(int i = 0; i < length; i++){
+            ary[i] = (int) (Math.random() * 9) + 1;
+        }
+        return ary;
     }
 
 }
