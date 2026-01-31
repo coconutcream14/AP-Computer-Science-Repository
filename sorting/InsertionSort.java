a9b46948727d326ecf3989b064fd508aeb0f21f2
Tanya Shah <staav410@gmail.com>
2026-01-31 21:20:29 +0000

    01/31 Finished code for Insertion sort

--- sorting/InsertionSort.java
+++ sorting/InsertionSort.java
@@ -2,7 +2,16 @@ package sorting;
 
 public class InsertionSort implements Sorter {
 
-    public void sort(int[] input) {
-        System.out.println("Insertion Sort!!!");
+    public void sort(int[] ary) {
+        for(int i = 1; i < ary.length; i++){
+            int key = ary[i];
+            int j = i - 1;
+
+            while(j >= 0 && key < ary[j]){
+                ary[j + 1] = ary[j];
+                j--;
+            }
+            ary[j + 1] = key;
+        }
     }
 }
